f75f71e14e862527cd65f5244f93d48f88258d24
snakehopper <snakehopper@gmail.com>
2016-03-08 20:22:00 +0800

    Customize intercom http client if provided

--- intercom.go
+++ intercom.go
@@ -2,6 +2,7 @@ package intercom
 
 import (
 	"github.com/intercom/intercom-go/interfaces"
+	"net/http"
 )
 
 // A Client manages interacting with the Intercom API.
@@ -57,10 +58,10 @@ func (c *Client) Option(opts ...option) (previous option) {
 	return previous
 }
 
-// NewClient returns a new Intercom API client, configured with the default HTTPClient.
-func NewClient(appID, apiKey string) *Client {
+// NewClient returns a new Intercom API client, configured with the default HTTPClient if tc==nil.
+func NewClient(tc *http.Client, appID, apiKey string) *Client {
 	intercom := Client{AppID: appID, APIKey: apiKey, baseURI: defaultBaseURI, debug: false, clientVersion: clientVersion}
-	intercom.HTTPClient = interfaces.NewIntercomHTTPClient(intercom.AppID, intercom.APIKey, &intercom.baseURI, &intercom.clientVersion, &intercom.debug)
+	intercom.HTTPClient = interfaces.NewIntercomHTTPClient(tc, intercom.AppID, intercom.APIKey, &intercom.baseURI, &intercom.clientVersion, &intercom.debug)
 	intercom.setup()
 	return &intercom
 }
